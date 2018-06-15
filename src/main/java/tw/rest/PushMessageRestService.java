c20fb35aecfcb0aa1b53aba03abba23aa5ef01e9
vanchhay <vanchhay>
2018-06-15 11:57:40 +0700

    POST request handling (returning POST json)

--- src/main/java/tw/rest/PushMessageRestService.java
+++ src/main/java/tw/rest/PushMessageRestService.java
@@ -1,15 +1,23 @@
 package tw.rest;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 
 @Path("/pushmessage")
 public class PushMessageRestService {
 
-	@POST
+	@GET
 	@Produces("application/json")
 	@Consumes("application/json")
 	public PushMessage getPushMessageDetail() {
@@ -17,5 +25,13 @@ public class PushMessageRestService {
 		PushMessage pushMessage = new PushMessage();
 
 		return pushMessage;
+	}
+
+	@POST
+	@Path("post")
+	@Produces("application/json")
+	@Consumes("application/json")
+	public Response addPushMessage(PushMessage pm) {
+		return Response.ok(pm).build();
 	}
 }
